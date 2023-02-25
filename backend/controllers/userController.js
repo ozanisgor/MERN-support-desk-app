0e5175e7c1d2dcc82805423f9035220f70363c18
ozan isgor <ozanisgor11@gmail.com>
2023-02-25 13:25:21 +0300

    add: Register User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,49 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+
+const User = require('../models/userModel')
 
 // @desc    Register a user
 // @route   /api/user
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
+
+  // Validation
   if (!name || !email || !password) {
     res.status(400)
     throw new Error('Please include all fields')
   }
-  res.send('Register Route')
+
+  //  Find if user already exist
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create User
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(400)
+    throw new error('Invalid user data')
+  }
 })
 
 // @desc    Login a user
