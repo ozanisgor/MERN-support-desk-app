22e96cf89a78c5ad12ebafee7da0b5a0c1b4b44b
ozan isgor <ozanisgor11@gmail.com>
2023-02-28 15:13:44 +0300

    add: Ticket Model & Routes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -8,7 +8,8 @@ const userSchema = mongoose.Schema(
     },
     email: {
       type: String,
-      required: [true, 'Please add an email']
+      required: [true, 'Please add an email'],
+      unique: true
     },
     password: {
       type: String,
