d945ef1baaa4b41b4a53cae3a03660244d148c03
ozan isgor <ozanisgor11@gmail.com>
2023-03-01 14:27:02 +0300

    add: Listing Tickets In UI

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { getTickets, reset } from '../features/tickets/ticketSlice'
 import Spinner from '../components/Spinner'
 import BackButton from '../components/BackButton'
+import TicketItem from '../components/TicketItem'
 
 function Tickets() {
   const { tickets, isLoading, isSuccess } = useSelector(state => state.tickets)
@@ -17,6 +18,25 @@ function Tickets() {
     return <Spinner />
   }
 
-  return <div>Tickets</div>
+  return (
+    <>
+      <BackButton url="/" />
+      <h1>Tickets</h1>
+      <div className="tickets">
+        <div className="ticket-headings">
+          <div>Data</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map(ticket => (
+          <TicketItem
+            key={ticket._id}
+            ticket={ticket}
+          />
+        ))}
+      </div>
+    </>
+  )
 }
 export default Tickets
