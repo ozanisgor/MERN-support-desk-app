59205c37d050c2e5e8d3a6434d190a02fa03a9b6
ozan isgor <ozanisgor11@gmail.com>
2023-03-02 14:42:04 +0300

    add: Submit a Note

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -5,7 +5,11 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useParams, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { getTicket, closeTicket } from '../features/tickets/ticketSlice'
-import { getNotes, reset as notesReset } from '../features/notes/noteSlice'
+import {
+  getNotes,
+  createNote,
+  reset as notesReset
+} from '../features/notes/noteSlice'
 import BackButton from '../components/BackButton'
 import Spinner from '../components/Spinner'
 import NoteItem from '../components/NoteItem'
@@ -59,7 +63,7 @@ function Ticket() {
   // Create note submit
   const onNoteSubmit = e => {
     e.preventDefault()
-    console.log('Submit')
+    dispatch(createNote({ noteText, ticketId }))
     closeModal()
   }
 
